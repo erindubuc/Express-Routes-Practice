83e7af1878b251f75f1948664d8ec5d3f2210df1
Erin Dubuc <erin.dubuc9@gmail.com>
2018-10-23 21:38:50 +0000

    created Express router and updated all status codes

---
+++ routes/partOne.js
@@ -0,0 +1,9 @@
+/*global swArray*/
+const express = require('express');
+const partOneRouter = express.Router();
+
+
+
+
+module.exports = partOneRouter;
+
--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
+const partOne = require('./routes/partOne');
+// on this route, use posts
+app.use('/routes/partOne', partOne)
 
 swArray = ['Storm Trooper', 'Darth Vader', 'Yoda', 'Luke', 'R2D2'];
 
@@ -12,50 +15,55 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //send a get request that displays my name
-app.get ('/', (req, res) => res.send('<h1>Erin</h1'))
+app.get ('/', (req, res) => {
+    res.send('<h1>Erin</h1');
+    // status 200 OK
+    res.status(200);
+});
 
-// create route that says something using the param sent to it
-// app.get('/pumpkin', (req, res) => res.send('<h1>Pumpkin Spice and Everything Nice</h1>'));
-
-// create query route ?= that accepts 2 nums 
+// GET route that says something using the param sent to it
+app.get('/pumpkin', (req, res) => {
+    res.send('<h1>Pumpkin Spice and Everything Nice</h1>');
+    // status 200 OK
+    res.status(200);
+});
+// GET query route ?= that accepts 2 nums 
 // should send back mathematical equation and the result 
 app.get('/divide', (req, res) => {
     const num1 = parseInt(req.query.num1, 10);
     const num2 = parseInt(req.query.num2, 10);
     const result = Math.floor(num1 / num2);
     res.send(`<h1>${num1} / ${num2} = ${result} (this number may be rounded)</h1>`);
+    // status 200 OK
+    res.status(200);
     });
 
-// Create a post route that accepts a username and password
-// if they match send a json with a status of "logged in" or respond with a json that has a status of "invalid credentials"
-// app.get('/login', (req, res) => {
-    // res.send(`<h1>You can log in now</h1>`);
-    // });
-    
+// POST route that accepts a username and password    
 app.post('/login/:name/:pass', (req, res) => {
     const username = req.params.name;
     const password = req.params.pass;
     
     // check to see if the username and password send match. 
     if (username !== "Chewbacca" && password !== "Wookie1979") {
-        res.json('invalid credentials');
+        // Status 403 Forbidden
+        res.status(403).json('invalid credentials');
         } else {
             res.status(200).json('logged in');  
             }
     });
 
-// create a global variable array
-// Create a post request that has an item attribute. 
+// POST request that has an item attribute. 
 app.post('/data/:item', (req, res) => {
     const item = req.params.item;
     const isItThere = swArray.indexOf(item);
-    console.log(swArray);
+    // console.log(swArray);
     
    if(isItThere == -1) {
         // Add the item to the array.
         swArray.push(item);
         console.log(swArray);
-        res.json({message: `${item} has been added to this array!`});
+        // Status 200 Ok
+        res.status(200).json({message: `${item} has been added to this array!`});
         }
          else { 
              // 409 = CONFLICT status code
@@ -63,18 +71,18 @@ app.post('/data/:item', (req, res) => {
             } 
     });
    
-// create a delete request that has an item attribute. 
-// remove the item from the array.indexOf() will find whether there is anything in the array.
+//DELETE request that has an item attribute. 
 app.delete('/data/:item', (req, res) => {
     const item = req.params.item;
     const isItThere = swArray.indexOf(item);
     
     if (isItThere != -1) {
             swArray.splice(isItThere, 1);
-            res.json(swArray);
+            // Status 200 Success with deletion
+            res.status(200).json(swArray);
             console.log(swArray);
             } else {
-            // the routes should respond back with an error if the item does not exist in the array 
+            // Status = error
              res.status(404).json(`${item} was not found in this array.`);
             } 
     });
